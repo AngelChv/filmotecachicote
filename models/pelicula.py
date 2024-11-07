e99896b12920b1e4a881193fbd9ea4b2ff6425c4
angel.chiveg <angel.chiveg@educa.jcyl.es>
2024-11-07 12:56:10 +0100

    Campos computados

--- models/pelicula.py
+++ models/pelicula.py
@@ -9,6 +9,7 @@ class pelicula(models.Model):
     _description = "filmotecachicote.pelicula"
 
     # Campos:
+    code = fields.Char(string="Código", compute="_get_code")
     name = fields.Char(string="Nombre", read_only=False, required=True, help="Nombre de la película")
     description = fields.Text(string="Descripción")
     film_date = fields.Date(string="Fecha")
@@ -41,4 +42,17 @@ class pelicula(models.Model):
 
     # Relaciones:
     genero_id = fields.Many2one("filmotecachicote.genero", string="Género", required=True, ondelete="cascade")
-    tecnicas_id = fields.Many2many("filmotecachicote.tecnica", string="Técnica", ondelete="cascade")
+    tecnicas_id = fields.Many2many("filmotecachicote.tecnica", string="Técnica", ondelete="cascade",
+                                   relation="tecnicas_pelicula",
+                                   column1="tecnicas_ids",
+                                   column2="peliculas_ids")
+
+
+    # Métodos:
+    # self en este caso no se refiere a sí mismo (una película) sino a toda la lista de películas.
+    def _get_code(self):
+        for pelicula in self:
+            if len(pelicula.genero_id) == 0:
+                pelicula.code = f"FILM_{pelicula.id}"
+            else:
+                pelicula.code = f"{pelicula.genero_id.name.upper()}_{pelicula.id}"
--- models/tecnica.py
+++ models/tecnica.py
@@ -9,4 +9,7 @@ class tecnica(models.Model):
     name = fields.Char(string="Nombre", read_only=False, required=True, help="Nombre de la técnica.")
     description = fields.Text(string="Descripción")
 
-    peliculas_id = fields.Many2many(String = "Películas", comodel_name="filmotecachicote.pelicula", ondelete="cascade")
+    peliculas_id = fields.Many2many(String="Películas", comodel_name="filmotecachicote.pelicula", ondelete="cascade",
+                                    relation="tecnicas_pelicula",
+                                    column1="peliculas_ids",
+                                    column2="tecnicas_ids")
